0ae4ce93dd961d48a2a9e366f9e5d30e3b83c41c
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-03-09 15:50:36 +0100

    feat: initialize bpmn-visualization demo (#2)
    Display a diagram and apply CSS classes to some BPMN elements.
    Diagram navigation is enabled.
    Display the version in the footer.

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,31 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+// this is simple example of the BPMN diagram, loaded as string. The '?.raw' extension support is provided by Vite.
+// for other load methods, see https://github.com/process-analytics/bpmn-visualization-examples
+import diagram from './diagram.bpmn?raw'
+import {BpmnVisualization, FitType} from 'bpmn-visualization';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// instantiate BpmnVisualization, pass the container HTMLElement - present in index.html
+const bpmnVisualization = new BpmnVisualization({
+    container: "bpmn-container",
+    navigation: {
+        enabled: true,
+    },
+});
+// load the BPMN diagram defined above
+bpmnVisualization.load(diagram, {fit: {type: FitType.Center, margin: 20}});
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// highlight task
+bpmnVisualization.bpmnElementsRegistry.addCssClasses(
+    [
+        "Activity_00vbm9s", // Record Goods Receipts
+        "Activity_1t65hvk", // Create Purchase Order Item
+    ],
+    "bpmn-highlight"
+);
+
+// display the bpmn-visualization version in the footer
+const footer = document.querySelector<HTMLElement>('footer')!;
+const version = bpmnVisualization.getVersion();
+const versionAsString = `bpmn-visualization@${version.lib}`;
+const dependenciesAsString = [...version.dependencies].map(([name, version]) => `${name}@${version}`).join('/');
+footer.innerText = `${versionAsString} with ${dependenciesAsString}`;
