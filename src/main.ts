61a9e9d50b2abb94f677e59449432fac124b01f1
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-03-09 17:17:55 +0100

    wip lint

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,31 @@
-import './style.css'
-// this is simple example of the BPMN diagram, loaded as string. The '?.raw' extension support is provided by Vite.
-// for other load methods, see https://github.com/process-analytics/bpmn-visualization-examples
-import diagram from './diagram.bpmn?raw'
+import './style.css';
 import {BpmnVisualization, FitType} from 'bpmn-visualization';
+// This is simple example of the BPMN diagram, loaded as string. The '?.raw' extension support is provided by Vite.
+// for other load methods, see https://github.com/process-analytics/bpmn-visualization-examples
+import diagram from './diagram.bpmn?raw';
 
-// instantiate BpmnVisualization, pass the container HTMLElement - present in index.html
+// Instantiate BpmnVisualization, pass the container HTMLElement - present in index.html
 const bpmnVisualization = new BpmnVisualization({
-    container: "bpmn-container",
-    navigation: {
-        enabled: true,
-    },
+  container: 'bpmn-container',
+  navigation: {
+    enabled: true,
+  },
 });
-// load the BPMN diagram defined above
+// Load the BPMN diagram defined above
 bpmnVisualization.load(diagram, {fit: {type: FitType.Center, margin: 20}});
 
-// highlight task
+// Highlight task
 bpmnVisualization.bpmnElementsRegistry.addCssClasses(
-    [
-        "Activity_00vbm9s", // Record Goods Receipts
-        "Activity_1t65hvk", // Create Purchase Order Item
-    ],
-    "bpmn-highlight"
+  [
+    'Activity_00vbm9s', // Record Goods Receipts
+    'Activity_1t65hvk', // Create Purchase Order Item
+  ],
+  'bpmn-highlight',
 );
 
-// display the bpmn-visualization version in the footer
+// Display the bpmn-visualization version in the footer
 const footer = document.querySelector<HTMLElement>('footer')!;
 const version = bpmnVisualization.getVersion();
 const versionAsString = `bpmn-visualization@${version.lib}`;
 const dependenciesAsString = [...version.dependencies].map(([name, version]) => `${name}@${version}`).join('/');
-footer.innerText = `${versionAsString} with ${dependenciesAsString}`;
+footer.textContent = `${versionAsString} with ${dependenciesAsString}`;
